b7b76c14673e8eb3bfa634b9920a81c0997ec44b
DanielTakeshi <takeshidanny@gmail.com>
2017-02-22 13:39:22 -0800

    Removed a lot of outdated matlab file, have one with mu_std = 0 now.

--- yutian_chen/tabulate_values.m
+++ yutian_chen/tabulate_values.m
@@ -6,10 +6,16 @@
 %   D: The number of points within [-K,K] to use for \mu_std values.
 %
 % What it saves:
-%   For now I will only be saving the `err` and `mean_j` terms. The `mean_j`
-%   value will be useful to compute \bar{\pi} estimates given the initial batch
-%   of, say, 100 elements. We can simply do 100 * mean_j. As for the error term,
-%   we'll figure out a good value to use empirically.
+%
+%   result(a,b,c) = (mu_std(a), err, mean_j)
+%
+%   where:
+%       a = mu_std index
+%       b = epsilon index (note: this is the value we pick for one
+%           accept/decision trial, it is NOT the error of the overall test
+%           (which is a _sequence_ of hypothesis tets))
+%       c = minibatch size (starting *and* the increment amount)
+%   (results is 4-D, the last index is 1, 2, or 3)
 %
 % Usage: 
 %   I just run it on my command line:
@@ -20,49 +26,60 @@
 %   watch out for the outfile_name! Set it to be something informative.
 % 
 % Notes:
-%   For logistic regression with MNIST(8M), we use a fixed minibatch size (and
-%   increment value) of 100. For regular MNIST, the training data has size 13000
-%   and for MNIST8M, we'll use 100k; otherwise anything larger means the
-%   competing methods will take absurdly long.
 %
-%   We have to fix an epsilon here (upper bound on error for each individual
-%   test). I will run with {0.001, 0.005, 0.01, 0.05, 0.1, 0.2} which roughly
-%   mirrors the values that were used in their paper. I'll run all of these
-%   together here.
-%
-%   I'll also be using their default discretization for bins.
+%   I'll be using their default discretization for bins.
 %
 %   CHECK THE OUTPUT FILE NAMES! Be careful to avoid over-writing stuff.
 
-% ratio: use 100/100000 for MNIST8M, 100/13000 for MNIST. Change file name.
-ratio = 100 / 13000; 
-K = 60;
-D = 25000;
-max_tests = 1. / ratio;
-assert(K > 0);
+% number_total_data: use 100000 for MNIST8M, 13000 for MNIST. Change file name!!
+number_total_data = 13000;
+K = 10;
 epsilons = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2];
-mu_std_values = linspace(-K, K, D);
+sizes = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
 
-for e = 1:length(epsilons)
-    result = zeros(D, 3);
-    eps = epsilons(e);
-    % CHECK for mnist, mnist8m, etc., in the file name!
-    outfile_name = sprintf('mu_std_K%d_D%d_mnist_eps%.4f.mat', K, D, eps)
-    fprintf('\tRunning tabulate_values using K = %d, D = %d\n', K, D);
-    fprintf('\tratio = %f, max_tests = %d, eps = %f\n', ratio, max_tests, eps);
+% If running for all mu_std values, set this to 0. Else, 1. This is mainly in
+% case I want to try finer-grained sizes and/or epsilons for the single mu_std=0
+% case. Otherwise with all the mu_std's, we can't afford to have too many
+% epsilon and size options for computational reasons.
+do_only_one = 1;
 
-    for i = 1:length(mu_std_values)
-        if mod(i,100) == 0
-            fprintf('i = %d, mu_std = %f\n', i, mu_std_values(i));
-        end
-        [err, mean_j, p_pos, p_neg, p_j] = seq_test_dynprog(mu_std_values(i), ...
-            ratio, max_tests, norminv(1 - eps));
-         result(i, 1) = mu_std_values(i);
-         result(i, 2) = err;
-         result(i, 3) = mean_j;
-    end
-
-    save(outfile_name, 'result', '-v7.3');
+if do_only_one == 1
+    D = 1;
+    mu_std_values = [0];
+    result = zeros(1, length(epsilons), length(sizes), 3);
+else 
+    D = 5000;
+    mu_std_values = linspace(-K, K, D);
+    result = zeros(D, length(epsilons), length(sizes), 3);
 end
+
+% CHANGE FILE NAME IF NEEDED!!
+outfile_name = sprintf('mu_std_K%d_D%d_mnist.mat', K, D)
+
+fprintf('\tRunning tabulate_values using K = %d, D = %d\n', K, D);
+for e = 1:length(epsilons)
+    eps = epsilons(e);
+
+    for j = 1:length(sizes)
+        m = sizes(j);
+
+        ratio = m / number_total_data; 
+        max_tests = ceil(1. / ratio);
+        fprintf('ratio = %f, max_tests = %d, eps = %f, m = %d\n', ...
+            ratio, max_tests, eps, m);
+
+        for i = 1:length(mu_std_values)
+            if mod(i,100) == 0
+                fprintf('i = %d, mu_std = %f\n', i, mu_std_values(i));
+            end
+            [err, mean_j, p_pos, p_neg, p_j] = seq_test_dynprog(mu_std_values(i), ...
+                ratio, max_tests, norminv(1 - eps));
+            result(i, e, j, 1) = mu_std_values(i);
+            result(i, e, j, 2) = err;
+            result(i, e, j, 3) = mean_j;
+        end
+    end
+end
+save(outfile_name, 'result', '-v7.3');
 
 fprintf('Done!\n');
